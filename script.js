6f909c06beb66b221dbd526fc42a060e0910d497
Taiyahm <taiyahm23@gmail.com>
2021-03-30 15:48:16 -0400

    added comments

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// create arrays to hold variable "libraries" for each character type
+
 var lower = "abcdefghijklmnopqrstuvwxyz";
 var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var numbers = "0123456789";
@@ -7,30 +7,35 @@ var symbols = "!$^&*-=+_?";
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+
+// Function that generates the password based on confirmed criteria
 function generatePW(){
+  // Prompts user to enters password length
 	var passwordLength = parseInt(prompt("Enter the length of your password between 8 and 128 characters:"));
 	
+
   if (passwordLength < 8) {
-    alert('The password must be at least 8 characters!');
+    alert('Must be at least 8 characters!');
     return;
   }
   if (passwordLength > 128) {
-    alert('The password must be less than 128 characters!');
+    alert('Must be less than 128 characters!');
     return;
   }
 
+  // Prompts user to include uppercase,lowercase,numbers, and/or symbols
+  var includeUpper = confirm("Would you like to include uppercase letters?");
+  var includeLower = confirm("Would you like to include lowercase letters?");
 	var includeNumber = confirm("Would you like to include numbers?");
 	var includeSymbols = confirm("Would you like to include symbols?");
-	var includeUpper = confirm("Would you like to include uppercase letters?");
-	var includeLower = confirm("Would you like to include lowercase letters?");
-		
-	if (!includeNumber && !includeSymbols && !includeUpper && !includeLower) {
-		alert('Must select at least one type of password character!');
+
+	// alerts user to select criteria if none is chosen
+	if (!includeNumber && !includeUpper && !includeSymbols && !includeLower) {
+		alert('Must select at least one of the criteria!');
 		return;
 	}	
 
-
-	var password = '';
+	var password = "";
 
 	for ( var i = 0; i < passwordLength; i++) {
 		if(includeNumber && password.length < passwordLength) {
