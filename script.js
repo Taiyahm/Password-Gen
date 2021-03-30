4a51b5a135e1d37ba1caf712f98b67c319b96c24
Taiyahm <taiyahm23@gmail.com>
2021-03-30 17:08:25 -0400

    updated Js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 var lower = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","u","z"];
 var upper = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 var numbers = ["0","1","2","3","4","5","6","7","8","9"];
-var symbols = ["!","$","^","&","*","-","=","+","_","?"];
+var symbols = ["!","#","$","^","&","*","-","=","+","_","?",];
 
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
